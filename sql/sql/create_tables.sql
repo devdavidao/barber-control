183a16cd6ba964a6430e212161daec096df4d004
devdavidao <david.henrique33@hotmail.com>
2025-11-30 18:27:09 -0300

    Update create_tables.sql

--- sql/sql/create_tables.sql
+++ sql/sql/create_tables.sql
@@ -1,1 +1,10 @@
+CREATE DATABASE BarberControl;
+USE BarberControl;
 
+CREATE TABLE Cliente (
+    id_cliente INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    telefone VARCHAR(20),
+    email VARCHAR(100)
+);
+...
